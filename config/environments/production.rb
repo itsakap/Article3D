0f58637c8cc71c926d15e6c667ad80eaeca6bec7
Adam Kaplan <AdamHKaplan@gmail.com>
2014-05-07 22:30:28 -0700

    precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Article3D::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.otf]
+   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif *.otf *.svg]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
